e08cad1dbcd0b33ad59534d40614b37fe629cf13
hoainammk3 <21020779@vnu.edu.vn>
2023-11-13 23:22:30 +0700

    finish merge ulist with search

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -7,15 +7,7 @@ var router = express.Router();
 const route = (app) => {
   app.use('/account', accountRouter)
   app.use('/ulist', ulistRouter);
-const foodRouter = require('./food/food.route');
-var router = express.Router();
-
-
-var route = (app) => {
-    app.use('/account', accountRouter)
-    app.use('/food', foodRouter)
-  
+  app.use('/food', foodRouter)
 }
-
 
 module.exports = route;
