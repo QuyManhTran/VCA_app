fc1752e5a788e9c1de9a888f29033bc29aef3930
longhainguyen <21020349@vnu.edu.vn>
2023-11-15 12:23:16 +0700

    fix verify user

--- server/src/controllers/ulist/ulist.js
+++ server/src/controllers/ulist/ulist.js
@@ -1,13 +1,13 @@
 const ulist = require('../../models/ulist/ulist');
 
 const createNewList = async (req, res, next) => {
-    const { name } = req.body;
+    const { name, id_user } = req.body;
     originalString = req.file.path;
     const startIndex = originalString.indexOf("public");
     const image = originalString.substring(startIndex);
     console.log(image);
-    
-    const newUList = new ulist({ name: name, image: image });
+
+    const newUList = new ulist({ id_user: id_user, name: name, image: image });
     try {
         await newUList.save();
         res.status(200).json({
@@ -18,7 +18,7 @@ const createNewList = async (req, res, next) => {
         if (error.code === 11000) {
             res.status(422).json({
                 success: false,
-                message: "name already exist"
+                message: "name already exist or id_user incorrect"
             })
         } else {
             res.status(500).json({
@@ -31,8 +31,9 @@ const createNewList = async (req, res, next) => {
 };
 
 const getAllList = async (req, res, next) => {
+    const { id_user } = req.body;
     try {
-        let allList = await ulist.find().sort({ createAt: -1 });
+        let allList = await ulist.find({ id_user: id_user }).sort({ createAt: -1 });
         res.status(200).json(allList);
         allList = null;
 
@@ -45,9 +46,10 @@ const getAllList = async (req, res, next) => {
 };
 
 const getSingleList = async (req, res, next) => {
-    const { id } = req.params;
+    const { id_user } = req.body;
+    const { id } = req.params; //id of list
     try {
-        let list = await ulist.findById(id);
+        let list = await ulist.find({ _id: id, id_user: id_user });
         res.status(200).json(list);
         list = null;
 
@@ -62,9 +64,10 @@ const getSingleList = async (req, res, next) => {
 };
 
 const deleteList = async (req, res, next) => {
+    const { id_user } = req.body;
     const { id } = req.params;
     try {
-        await ulist.deleteOne({ _id: id });
+        await ulist.deleteOne({ _id: id, id_user: id_user });
         res.status(200).json({
             success: true,
             message: "Delete Success"
@@ -78,14 +81,15 @@ const deleteList = async (req, res, next) => {
 };
 
 const editNameList = async (req, res, next) => {
-    const { name } = req.body;
+    const { name, id_user } = req.body;
     const { id } = req.params;
     try {
         let list = await ulist.findOneAndUpdate(
-            { _id: id },
+            { _id: id, id_user: id_user },
             { $set: { name: name } },
             { upsert: true },
         )
+
 
         if (list) {
             // Update successful
@@ -102,10 +106,11 @@ const editNameList = async (req, res, next) => {
         }
 
     } catch (error) {
+        console.log(error.code);
         if (error.code === 11000) {
             res.status(422).json({
                 success: false,
-                message: "name already exist"
+                message: "id_user incorrect"
             })
         } else {
             res.status(400).json({
@@ -119,56 +124,84 @@ const editNameList = async (req, res, next) => {
 
 const addItemOfList = async (req, res, next) => {
     const { id } = req.params;
-    const { idFood, imageFood } = req.body;
-    let list = await ulist.findById(id);
-    const updateList = [...list.list, {
-        id_food: idFood,
-        image: imageFood
-    }]
-
-
+    const { idFood, imageFood, id_user } = req.body;
     try {
-        let list = await ulist.findOneAndUpdate(
-            { _id: id },
-            { $set: {list : updateList}},
-            { upsert: true },
-        );
 
-        if (list) {
-            // Update successful
-            res.status(200).json({
-                success: true,
-                message: "Update list success",
-            });
-        } else {
-            // Document not found, handle accordingly
+        let list = await ulist.find({ _id: id, id_user: id_user });
+        const old_list_food = [...list[0].list]
+        let isIdFoodExsit = false;
+        for (let i = 0; i < old_list_food.length; i++) {
+            if (old_list_food[i].id_food == idFood) {
+                isIdFoodExsit = true;
+                break;
+            }
+        }
+        if (isIdFoodExsit) {
+            return res.status(422).json({
+                success: false,
+                message: "name already exist"
+            })
+        }
+        const updateList = [...list[0].list, {
+            id_food: idFood,
+            image: imageFood
+        }];
+
+        if (!list) {
             res.status(404).json({
                 success: false,
                 message: "Document not found",
             });
         }
+        try {
+            let list = await ulist.findOneAndUpdate(
+                { _id: id, id_user: id_user },
+                { $set: { list: updateList } },
+                { upsert: true },
+            );
 
-        list = null;
+            if (list) {
+                res.status(200).json({
+                    success: true,
+                    message: "Update list success",
+                });
+            } else {
+                res.status(404).json({
+                    success: false,
+                    message: "Document not found",
+                });
+            }
+
+            list = null;
+        } catch (error) {
+            res.status(400).json({
+                success: false,
+                message: "Fail"
+            })
+        }
+
+
     } catch (error) {
-        res.status(400).json({
-            success: false,
-            message: "Fail"
-        })
-    }
 
+        res.status(500).json({
+            success: false,
+            message: "id_user incorrect or not found id of list"
+        })
+
+    }
 };
 
 const deleteItemOfList = async (req, res, next) => {
     const { id } = req.params;
-    const { idFood } = req.body
-    let list = await ulist.findById(id);
-    updateList = [...list.list]
-    console.log(updateList);
+    const { idFood, id_user } = req.body;
+
+    let list = await ulist.find({_id : id, id_user:id_user});
+    updateList = [...list[0].list];
     updateList = updateList.filter(item => item.id_food !== idFood)
 
     try {
         let list = await ulist.findOneAndUpdate(
-            { _id: id },
+            { _id: id, id_user:id_user },
             { $set: { list: updateList } },
             { upsert: true },
         );
--- server/src/models/ulist/ulist.js
+++ server/src/models/ulist/ulist.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UList = new Schema({
+    id_user : {
+        type: String,
+        require:true
+    },
     name: {
+        require:true,
         type: String,
         unique: true,
     },
