3f20cda5b9d03171419b6daec9cb5bca13150bdc
longhainguyen <21020349@vnu.edu.vn>
2023-11-25 16:12:34 +0700

    history api fix query and text

--- server/src/controllers/ulist/historyController.js
+++ server/src/controllers/ulist/historyController.js
@@ -2,33 +2,37 @@ const food = require("../../models/food/food");
 const history = require("../../models/ulist/history");
 
 const getSortedWatchedFoods = async (req, res) => {
-    const { id_user } = req.body;
-    const userHistory = await history.findOne(
-        { id_user: id_user },
-        { watchedFoods: 1 }
-    )
+    const { id_user } = req.query;
 
-    if (!userHistory) {
-        return res.status(404).json({ message: "User history not found" });
+    try {
+        const userHistory = await history.findOne(
+            { id_user: id_user },
+            { watchedFoods: 1 }
+        )
+    
+        const dataWatchedFoods = await Promise.all(userHistory.watchedFoods.map(async (watchedFood) => {
+            const detailWatchedFood = await food.findOne({
+                _id: watchedFood.food.id_food
+            })
+            return {
+                id_food: detailWatchedFood._id,
+                name: detailWatchedFood.name,
+                like: detailWatchedFood.like,
+                rate: detailWatchedFood.rate,
+                image: detailWatchedFood.image,
+                watchedAt: watchedFood.watchedAt,
+            }
+        }));
+    
+        dataWatchedFoods.sort((a, b) => b.watchedAt - a.watchedAt);
+    
+        res.status(200).json({ dataWatchedFoods: dataWatchedFoods });
+    } catch (error) {
+       
+        return res.status(404).json({ text: "Không tìm thấy tài khoản" });
+        
     }
 
-    const dataWatchedFoods = await Promise.all(userHistory.watchedFoods.map(async (watchedFood) => {
-        const detailWatchedFood = await food.findOne({
-            _id: watchedFood.food.id_food
-        })
-        return {
-            id_food: detailWatchedFood._id,
-            name: detailWatchedFood.name,
-            like: detailWatchedFood.like,
-            rate: detailWatchedFood.rate,
-            image: detailWatchedFood.image,
-            watchedAt: watchedFood.watchedAt,
-        }
-    }));
-
-    dataWatchedFoods.sort((a, b) => b.watchedAt - a.watchedAt);
-
-    res.status(200).json({ dataWatchedFoods: dataWatchedFoods });
 }
 
 const addWatchedFoodToHistory = async (req, res) => {
@@ -60,7 +64,9 @@ const addWatchedFoodToHistory = async (req, res) => {
                 }
             );
 
-            return res.status(200).json("Cập nhật lịch sử thành công");
+            return res.status(200).json({
+                text: "Cập nhật lịch sử thành công"
+            });
         } else {
             await history.updateOne(
                 { id_user: id_user },
@@ -75,10 +81,14 @@ const addWatchedFoodToHistory = async (req, res) => {
                 }
             );
 
-            return res.status(200).json("Thêm vào lịch sử thành công");
+            return res.status(200).json({
+                text: "Thêm vào lịch sử thành công"
+            });
         }
     } catch (error) {
-        return res.status(404).json("Không tìm thấy tài khoản");
+        return res.status(404).json({
+            text : "Không tìm thấy tài khoản"
+        });
     }
 }
 
