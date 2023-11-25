7ea7b96303e9e13b406cd76294ea6c1c30ef5506
longhainguyen <21020349@vnu.edu.vn>
2023-11-25 16:01:19 +0700

    history api done v1

--- server/src/controllers/ulist/historyController.js
+++ server/src/controllers/ulist/historyController.js
@@ -2,10 +2,10 @@ const food = require("../../models/food/food");
 const history = require("../../models/ulist/history");
 
 const getSortedWatchedFoods = async (req, res) => {
-    const {id_user} = req.body;
+    const { id_user } = req.body;
     const userHistory = await history.findOne(
-        {id_user: id_user},
-        {watchedFoods: 1}
+        { id_user: id_user },
+        { watchedFoods: 1 }
     )
 
     if (!userHistory) {
@@ -14,7 +14,7 @@ const getSortedWatchedFoods = async (req, res) => {
 
     const dataWatchedFoods = await Promise.all(userHistory.watchedFoods.map(async (watchedFood) => {
         const detailWatchedFood = await food.findOne({
-            _id : watchedFood.food.id_food
+            _id: watchedFood.food.id_food
         })
         return {
             id_food: detailWatchedFood._id,
@@ -32,38 +32,56 @@ const getSortedWatchedFoods = async (req, res) => {
 }
 
 const addWatchedFoodToHistory = async (req, res) => {
-    const {id_user, id_food} = await req.body;
-    const isWatchedFood = await history.find(
-        {
-            id_user: id_user,
-            watchedFoods: {
-                $elemMatch: {
-                  food : {id_food: id_food}
-                }
-             }
-        }
-    );
-
-    console.log(isWatchedFood);
-
-    if (isWatchedFood) {
-
-    }
-
     try {
-        await history.updateOne(
-            {id_user: id_user},
-            {$push: {watchedFoods:{food: {
-                id_food: id_food
-            }}}}
-        )
-        res.json("200 ok");    
-    } catch (error) {
-        res.json("400 error")
-    }
-    
+        const { id_user, id_food } = req.body;
 
+        const userHistory = await history.findOne({
+            id_user: id_user,
+            'watchedFoods.food.id_food': id_food
+        });
+
+        if (userHistory) {
+
+            await history.updateOne(
+                { id_user: id_user },
+                { $pull: { 'watchedFoods': { 'food.id_food': id_food } } }
+            );
+
+            await history.updateOne(
+                { id_user: id_user },
+                {
+                    $push: {
+                        watchedFoods: {
+                            food: {
+                                id_food: id_food
+                            }
+                        }
+                    }
+                }
+            );
+
+            return res.status(200).json("Cập nhật lịch sử thành công");
+        } else {
+            await history.updateOne(
+                { id_user: id_user },
+                {
+                    $push: {
+                        watchedFoods: {
+                            food: {
+                                id_food: id_food
+                            }
+                        }
+                    }
+                }
+            );
+
+            return res.status(200).json("Thêm vào lịch sử thành công");
+        }
+    } catch (error) {
+        return res.status(404).json("Không tìm thấy tài khoản");
+    }
 }
+
 
 module.exports = {
     getSortedWatchedFoods,
